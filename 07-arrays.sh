0d291136b863663131f13d4e5d9b772e03534b5b
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 00:49:54 +0530

    Now we are discussing Array

---
+++ 07-arrays.sh
@@ -0,0 +1,19 @@
+#!/bin/bash/
+
+# Now we are discussing about arrays
+
+# syntax: arrayname=(<here you mention the list with double codes>)
+
+MOVIES=("Avatar" "Inception" "Titanic")
+
+# size of the array start with 1
+# index of the array start with 0
+# in shell script @ means everything
+
+echo "First movie name is : ${MOVIES[0]}"
+
+echo "Second movie name is : ${MOVIES[1]}
+
+echo "Third movie name is : ${MOVIES[2]}"
+
+echo "@ means everything now its print everything : ${MOVIES[@]}"
