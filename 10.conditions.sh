583ca1124c1243d0bb4b15fcfc1e206a6bf9f2f1
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 02:13:35 +0530

    about Data Types

---
+++ 10.conditions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## Now we are discussing about conditions
+
+#   if(condition)            # after if inside the braces we called as condition
+#       then
+#            echo "that condition is true print this statement"
+
+#   else 
+#            echo "above condition is false print else statement"
+
+#   fi              # in shell script (fi) is closing the condition
+
+# Question:: Write the program which one is greaterthan
+
+NUMBER=$1
+
+if ($NUMBER -gt 10)
+then
+    echo "The Entered  $NUMBER is greaterthan 10"
+
+else 
+    echo "The entered $NUMBER is lessthan 10"
+
+fi
