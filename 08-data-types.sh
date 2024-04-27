486f2080d64a67c216536bcb198a7393aa3ea885
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 01:16:29 +0530

    about Data Types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -6,6 +6,11 @@
 Num1=$1
 Num2=$2
 
-SUM=$($Num1+$Num2)
+SUM=$(($Num1+$Num2))
+
+# Here we need to mention two braces (()) 
+# inside braces we need to mention the condition
+# Before condition we need to mention $ then only it is considered as Dolar
+# like $((mention the condition))
 
 echo "Here we are adding two numbers total sum is : $Sum"
