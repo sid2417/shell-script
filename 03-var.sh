38a46fc134d205341d27ee4d139a275a8c645c1e
sid2417 <siddhartha.java9398@gmail.com>
2024-04-27 17:35:51 +0530

    Just Pull

---
+++ 03-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+Person1=Happy
+Person2=Joy
+
+echo "$Person1:: Hi $Person2, How are you"
+echo "$Person2:: Hello $Person1, am fine, where are you going"
+echo "$Person1:: I am going to library "
+echo "$Person2:: Ohh, its ok, am going to theater"
