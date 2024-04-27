78ad3e6de091bd38bee19bc97bb87ddd54dc6e7d
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 01:02:54 +0530

    about arrays

---
+++ 08-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# now we are dealing with Data types
+# Here we are passing arguments from outside
+
+Num1=$1
+Num2=$2
+
+echo "Here we are adding two numbers total sum is : $Sum=($1+$2)"
