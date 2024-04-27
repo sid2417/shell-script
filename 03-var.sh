87f69c879fcd07717a74cbc9caab10afb4e46576
sid2417 <siddhartha.java9398@gmail.com>
2024-04-27 17:55:05 +0530

    Just Pull

--- 03-var.sh
+++ 03-var.sh
@@ -1,10 +1,18 @@
 #!/bin/bash/
 
-Person1=Yoga ###Here no space between variable=value
-Person2=Exercise
+#Declaring variable
+Person1=$1       ###Here no space between variable=value
+Person2=$2  
+
+#Here $1 considered as first person
+#Here $2 considered as second person
+
+#while execute the srcipt you provide the names from outside like a arguments
 
 ## $ dollar is used for to identify the variable 
+##Now you have to chage the value into any name
 
+#Referring variable
 echo "$Person1:: Hi $Person2, How are you"
 echo "$Person2:: Hello $Person1, am fine, where are you going"
 echo "$Person1:: I am going to library "
