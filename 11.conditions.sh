4d62f300165491c8a85654417e63ba7cbc1f879f
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 02:31:25 +0530

    about Data Types

--- 11.conditions.sh
+++ 11.conditions.sh
@@ -1,14 +1,9 @@
-#!/bin/bash/
+#!/bin/bash
 
 
 NUMBER=$1
 if [ $NUMBER -eq 10 ]
     then 
         echo "Given number $NUMBER is equal to : 10 "
-            if [ $NUMBER -gt 10]
-                then 
-                    echo "The Given number $NUMBER is greaterthan 10"
-                else
-                    echo "The Given number $NUMBER is lessthan 10"
-            fi
+            
 fi
