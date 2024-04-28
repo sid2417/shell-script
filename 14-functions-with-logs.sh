2fd52182ccf3ff5f7b8960fdbf840d146a092fff
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 21:37:48 +0530

    Functions

--- 14-functions-with-logs.sh
+++ 14-functions-with-logs.sh
@@ -5,7 +5,7 @@
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
-LOG_FILE=/tmp/($SCRIPT_NAME-$TIMESTAMP.log)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 USERID=$(id -u)
