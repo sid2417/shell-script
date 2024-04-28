7daf469d4d0588acd6b80e3f39f7ab3caf4897f4
sid2417 <siddhartha.java9398@gmail.com>
2024-04-28 16:15:33 +0530

    installing mysql with srcipt

--- 12.mysql-installation.sh
+++ 12.mysql-installation.sh
@@ -1,23 +1,30 @@
 
 #!/bin/bash/
 
-VARIABLE=$(id -u)
+# where ever we are getting failure manually we provide the exit
 
-if [ $VARIABLE -ne 0 ]
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
 then 
-    echo "Please provide the sudo access"
+    echo "Please provide the SUDO ACCESS"
     # if it is not a super user we need to break
+    exit 1 # Here you need to mention 1 to 127 upto
+    # in suppose you mentioned exit 0 it is considered as success,
+    # exit 0 is continue the srcipt
 else
-    echo "You are a super user"
+    echo "You are a SUPER USER"
 
 fi
 
-dnf install mysql-server
+dnf install mysql-server -y   #Here -y is mandatory
 
-if [ $? -eq 0 ]
+
+if [ $? -ne 0 ]
 then 
-    echo "Your installation is SUCCESS"
-    #If it is successfully installed we need to break
+     echo "Your installation is FAILED"  
+    exit 1
 else
-    echo "Your installation is Failed"
+   
+    echo "Your installation is SUCCESS"
 fi
