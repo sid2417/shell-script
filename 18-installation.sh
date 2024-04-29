205691774129f6bc754968187aea89b6185a35f8
sid2417 <siddhartha.java9398@gmail.com>
2024-04-29 15:20:13 +0530

    installation for practise

--- 18-installation.sh
+++ 18-installation.sh
@@ -4,7 +4,7 @@
 USER_ID=$(id -u)
 if [ $USER_ID -eq 0 ]
 then 
-    echo -e "$G YOU ALREADY HAVE SUDO ACCESS $N"
+    echo -e " $G YOU ALREADY HAVE SUDO ACCESS $N "
 else
     echo -e "$Y PLEASE PROVIED SUDO ACCESS $N"
 fi
