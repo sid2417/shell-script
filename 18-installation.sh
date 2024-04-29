aada893fb4de979c26e05fac7c762194e37f716f
sid2417 <siddhartha.java9398@gmail.com>
2024-04-29 15:47:05 +0530

    installation for practise

--- 18-installation.sh
+++ 18-installation.sh
@@ -2,24 +2,27 @@
 
 #sudo access 
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SRCIPT_NAME=$(echo "$0" | cut -d "." -f1)
+LOG_FILE=/tmp/$SRCIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
-    echo -e " $G Please Provide Sudo Access"
+    echo -e " $G Please Provide Sudo Access $N"
     exit 1
 else
-    echo -e " $Y Already You Have Sudo Access "
+    echo -e " $Y Already You Have Sudo Access $N"
 fi
 
 #colors
 
 
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
 
 
 #Timestamp #logfile   #chech list installed 
