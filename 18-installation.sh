1ac82b8d6a9f4163dc21157075dd42be02d14c86
sid2417 <siddhartha.java9398@gmail.com>
2024-04-29 18:49:28 +0530

    installation for practise

--- 18-installation.sh
+++ 18-installation.sh
@@ -1,24 +1,16 @@
 #!/bin/bash/
 
+USER_ID=$(id -u)
+TIME_STAMP=$(date +%F-%H-%M-%S)
+#logfile  
+SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
+LOG_FILE=/tmp/$TIME_STAMP-$SCRIPT_NAME.log
 #colors
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 #Timestamp 
-TIME_STAMP=$(date +%F-%H-%M-%S)
-#logfile  
-SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
-LOG_FILE=/tmp/$TIME_STAMP-$SCRIPT_NAME.log
-
-USER_ID=$(id -u)
-if [ $USER_ID -eq 0 ]
-    then 
-    echo -e " $G YOU ALREADY HAVE SUDO ACCESS $N "
-    else
-    echo -e "$R PLEASE PROVIDE SUDO ACCESS $N "
-    exit 1
-fi
 
 VALIDATE ()
 {   if ($1 -ne 0)
@@ -30,17 +22,29 @@ VALIDATE ()
     fi
 }
 
+if [ $USER_ID -ne 0 ]
+    then 
+        echo -e "$R PLEASE PROVIDE SUDO ACCESS $N "
+        exit 1
+    else
+    echo -e " $G YOU ALREADY HAVE SUDO ACCESS $N "  
+fi
+
+
+
 for i in $@
 do 
     ehco -e " $Y The installed Package Name is : $i $N"
     dnf list installed $i &>>$LOG_FILE
-    if [ $? -ne 0 ]
+    if [ $? -eq 0 ]
     then 
+        echo -e " $G $i was Already INSTALLED $N"    
         #echo -e " $Y $i was NOT INSTALLED...Now we proceed to install $Y"
+        
+        
+    else 
         dnf install $i -y &>>$LOG_FILE
         VALIDATE $? "$i package"
-    else 
-        echo -e " $G $i was Already INSTALLED $N"    
     fi
 done
 
