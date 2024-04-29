23624c948d99213bcfa3abbb2f9d15fa2ae47146
sid2417 <siddhartha.java9398@gmail.com>
2024-04-29 15:06:13 +0530

    installation for practise

---
+++ 18-installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+
+#sudo access 
+USER_ID=$(id -u)
+if [ $? -eq 0 ]
+then 
+    echo -e "$G YOU ALREADY HAVE SUDO ACCESS $N"
+else
+    echo "$YPLEASE PROVIED SUDO ACCESS$N"
+fi
+
+#colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#Timestamp #logfile   #chech list installed 
+#check installation was success/Failed
+
+
+
+
